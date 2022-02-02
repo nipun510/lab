d9d6f01a6ccda3098fbf69b4aecb71fce6e26da5
nipun510 <nipunkumar510@gmail.com>
2022-02-02 13:20:01 +0530

    enhanced json lib

--- cpp/json.h
+++ cpp/json.h
@@ -13,14 +13,44 @@
 #include <concepts>
 #include <cassert>
 
-class json{
+class json;
+class jsonValue;
+
+class jsonEncoder 
+{
+  public:
+    virtual json*       loads(const std::string &str) const = 0;
+    virtual std::string dumps(const json *jsonObj) const = 0;
+
+    virtual jsonEncoder * clone() = 0;
+};
+
+class jsonEncoderImp  : public jsonEncoder
+{
+  public:
+    std::string dumps(const json *jsonObj) const override;
+    json* loads(const std::string &str) const override {return nullptr;}
+
+  private:
+    void dumpUtil(const jsonValue *jsonObj, std::ostringstream &os) const;
+    virtual jsonEncoder * clone() override {
+      return new jsonEncoderImp();
+    }
+};
+
+
+class jsonValue 
+{
   public:
     using boolType = bool;
     using nullType = std::nullptr_t;
     using numberType = std::variant<int, double>;
     using stringType = std::string;
-    using arrayType = std::vector<json*>;
-    using objectType = std::map<std::string, json*>;
+    using arrayType = std::vector<jsonValue*>;
+    using objectType = std::map<std::string, jsonValue*>;
+    using jsonValueType = std::variant<boolType, int, double, stringType, nullType, arrayType, objectType>;
+
+
     template<typename T>
     struct isValueType : std::integral_constant<bool, 
                              std::is_same_v<T, boolType> ||
@@ -30,90 +60,156 @@ class json{
                              std::is_same_v<T, arrayType> ||
                              std::is_same_v<T, objectType>>{};
     template<typename T>
-    static inline constexpr bool isValueType_v = isValueType<T>::value;
+    static constexpr bool isValueType_v = isValueType<T>::value;
 
 
-    static constexpr std::nullptr_t nullValue = std::nullptr_t{};
-    std::string dump() const;
+    using jsonArrayIter = arrayType::iterator;
+    using jsonObjectIter = objectType::iterator;
+    using jsonArrayConstIter = arrayType::const_iterator;
+    using jsonObjectConstIter = objectType::const_iterator;
 
-    json() = default;
-    explicit json(int val) : _value{val} {}
-    explicit json(bool val) : _value{val} {}
-    explicit json(const std::string &val) : _value{val} {}
-    explicit json(const char * val) : json(std::string(val)){}
-    explicit json(const std::nullptr_t &val) : _value{val} {}
+    explicit jsonValue(int val) : _value{val} {}
+    explicit jsonValue(bool val) : _value{val} {}
+    explicit jsonValue(const std::string &val) : _value{val} {}
+    explicit jsonValue(const char * val) : jsonValue(std::string(val)){}
+    explicit jsonValue(const std::nullptr_t &val) : _value{val} {}
+
+    ~jsonValue() 
+    {
+      if (isValueArrayType()) {
+        for (jsonValue *value : std::get<arrayType>(_value)) {
+          delete value;
+        }
+      }
+      if (isValueObjectType()) {
+        for (auto &value : std::get<objectType>(_value)) {
+          delete value.second;
+        }
+      }
+    }
 
     template<typename T>
-    json (const std::vector<T> &arr) 
+    jsonValue (const std::vector<T> &arr) 
     {
       _value = arrayType{};
       for (const auto & elem : arr) {
-        std::get<arrayType>(_value).push_back(new json(elem));
+        std::get<arrayType>(_value).push_back(new jsonValue(elem));
       }
     }
-    
 
     template<typename T>
-    json (const std::map<std::string, T> &obj) 
+    jsonValue (const std::map<std::string, T> &obj) 
     {
       _value = objectType{};
       for (const auto & elem : obj) {
-        std::get<objectType>(_value).insert({elem.first, new json(elem.second)});
+        std::get<objectType>(_value).insert({elem.first, new jsonValue(elem.second)});
       }
     }
+
+    bool isValueBoolType() const{ return std::holds_alternative<boolType>(_value); }
+    bool isValueNullType() const{ return std::holds_alternative<nullType>(_value); }
+    bool isValueNumberType() const { return isValueIntType() || isValueDoubleType(); }
+    bool isValueStringType() const { return std::holds_alternative<stringType>(_value); }
+    bool isValueArrayType() const { return std::holds_alternative<arrayType>(_value); }
+    bool isValueObjectType() const { return std::holds_alternative<objectType>(_value); }
+    bool isValueIntType() const { return std::holds_alternative<int>(_value); }
+    bool isValueDoubleType() const { return std::holds_alternative<double>(_value); }
+
+    jsonArrayConstIter arrayConstBegin() const{ return std::get<arrayType>(_value).cbegin(); }
+    jsonArrayConstIter arrayConstEnd() const{ return std::get<arrayType>(_value).cend(); }
+    jsonObjectConstIter objectConstBegin() const { return std::get<objectType>(_value).cbegin(); }
+    jsonObjectConstIter objectConstEnd() const { return std::get<objectType>(_value).cend(); }
+    jsonArrayIter arrayBegin() { return std::get<arrayType>(_value).begin(); }
+    jsonArrayIter arrayEnd() { return std::get<arrayType>(_value).end(); }
+    jsonObjectIter objectBegin() { return std::get<objectType>(_value).begin(); }
+    jsonObjectIter objectEnd() { return std::get<objectType>(_value).end(); }
+    std::string stringValue() const{ return std::get<stringType>(_value); }
+    int intValue() const{ return std::get<int>(_value); }
+    double doubleValue() const{ return std::get<double>(_value); }
+    bool boolValue() const{ return std::get<boolType>(_value); }
+
 
     template<typename T>
       requires isValueType_v<T>
     void insert(const std::pair<std::string, T> &elem) 
     {
-      objectType::iterator it;
+      assert(isValueObjectType());
       if (isValueObjectType()) {
-        std::get<objectType>(_value).insert(std::make_pair(elem.first, new json(elem.second)));
+        std::get<objectType>(_value).insert(std::make_pair(elem.first, new jsonValue(elem.second)));
       }
     }
 
-
-    bool isValueBoolType() const{
-      return std::holds_alternative<boolType>(_value);
-    }
-    bool isValueNullType() const{
-      return std::holds_alternative<nullType>(_value);
-    }
-    bool isValueNumberType() const
-    {
-      return isValueIntType() || isValueDoubleType();
-    }
-
-    bool isValueStringType() const
-    {
-      return std::holds_alternative<stringType>(_value);
-    }
-
-    bool isValueArrayType() const
-    {
-      return std::holds_alternative<arrayType>(_value);
-    }
-
-    bool isValueObjectType() const
-    {
-      return std::holds_alternative<objectType>(_value);
-    }
-
-    bool isValueIntType() const
-    {
-        return std::holds_alternative<int>(_value);
-    }
-
-    bool isValueDoubleType() const
-    {
-        return std::holds_alternative<double>(_value);
-    }
-
   private:
-    void dumpUtil(std::ostringstream &os) const;
+    jsonValueType _value;
+};
+
+
+
+class json
+{
+  public:
+    // TYPES
+    using nullType = jsonValue::nullType;
+    static constexpr std::nullptr_t nullValue = std::nullptr_t{};
+
+    // CONSTRUCTORS
+    json() = default;
+    explicit json(int val) : _value{new jsonValue{val}} {}
+    explicit json(bool val) : _value{new jsonValue{val}} {}
+    explicit json(const std::string &val) : _value{new jsonValue{val}} {}
+    explicit json(const char * val) : json(new jsonValue{val}){}
+    explicit json(const std::nullptr_t &val) : _value{new jsonValue{val}} {}
+    
+    template<typename T>
+    json (const std::vector<T> &arr) : _value{new jsonValue{arr}}{}
+    template<typename T>
+    json (const std::map<std::string, T> &obj) : _value{new jsonValue{obj}} {} 
+
+
+    // DESTRUCTOR
+    ~json() {
+      delete _value;
+      delete _encoder;
+    }
+
+    // MANUPULATORS
+    void setEncoder(jsonEncoder *encoder) 
+    {
+      assert(encoder);
+      if (_encoder) {
+        delete _encoder;
+        _encoder = nullptr;
+      }
+      _encoder = encoder->clone();
+    }
+
+    template<typename T>
+      requires jsonValue::isValueType_v<T>
+    void insert(const std::pair<std::string, T> &elem) 
+    {
+      _value->insert(elem);
+    }
+
+    // ACCESSORS
+    const jsonValue *getValue() const; 
+    std::string dumps() const;
+
+    bool isValueBoolType() const{ return _value->isValueBoolType();}
+    bool isValueNullType() const{ return _value->isValueNullType();}
+    bool isValueNumberType() const{ return _value->isValueNumberType();}
+    bool isValueIntType() const{ return _value->isValueIntType();}
+    bool isValueDoubleType() const{ return _value->isValueDoubleType();}
+    bool isValueStringType() const{ return _value->isValueStringType();}
+    bool isValueArrayType() const{ return _value->isValueArrayType();}
+    bool isValueObjectType() const{ return _value->isValueObjectType();}
+
+
+    friend class jsonEncoder;
+  private:
 
     // Data
-    std::variant<boolType, int, double, stringType, nullType, arrayType, objectType> _value;
+    jsonValue *_value {nullptr};
+    jsonEncoder *_encoder = new jsonEncoderImp();
 };
 
 
@@ -121,44 +217,58 @@ class json{
 
 // inline functions definations
 std::string
-json::dump() const
+inline json::dumps() const
+{
+  return _encoder->dumps(this);
+}
+
+inline  const jsonValue *
+json::getValue() const
+{
+  return _value;
+}
+
+std::string
+inline jsonEncoderImp::dumps(const json *jsonObj) const
 {
   std::ostringstream os;
-  dumpUtil(os);
+  dumpUtil(jsonObj->getValue(), os);
   return os.str();
 }
 
-void
-json::dumpUtil(std::ostringstream &os) const
+inline void
+jsonEncoderImp::dumpUtil(const jsonValue *value, 
+                                std::ostringstream &os) const
 {
-  if(isValueIntType()) {
-    os << std::to_string(std::get<int>(_value));
-  } else if(isValueDoubleType()) {
-    os << std::to_string(std::get<double>(_value));
-  } else if (isValueArrayType()) {
+  // TODO implements this with std::visit
+  if(value->isValueIntType()) {
+    os << std::to_string(value->intValue());
+  } else if(value->isValueDoubleType()) {
+    os << std::to_string(value->doubleValue());
+  } else if (value->isValueArrayType()) {
     os << "[";
     const char* separator = "";
-    for (const auto & elem : std::get<arrayType>(_value)) {
+    for (auto it = value->arrayConstBegin(); it != value->arrayConstEnd(); ++it) {
       os << separator << " ";
-      elem->dumpUtil(os);
+      dumpUtil(*it, os);
       separator = ", ";
     }
     os << "]";
-  } else if (isValueObjectType()) {
+  } else if (value->isValueObjectType()) {
     os << "{";
     const char* separator = "";
-    for (const auto & elem : std::get<objectType>(_value)) {
-      os << separator << "{" << elem.first << " : ";
-      elem.second->dumpUtil(os);
+    for (auto it = value->objectConstBegin(); it != value->objectConstEnd(); ++it) {
+      os << separator << "{" << it->first << ": ";
+      dumpUtil(it->second, os);
       os << "}";
       separator = ", ";
     }
     os << "}";
-  } else if (isValueBoolType()) {
-    os << (std::get<bool>(_value) == true ? "true" : "false");
-  }  else if (isValueStringType()) {
-      os << std::get<std::string>(_value);
-  } else if (isValueNullType()) {
+  } else if (value->isValueBoolType()) {
+    os << (value->boolValue() == true ? "true" : "false");
+  } else if (value->isValueStringType()) {
+    os << value->stringValue();
+  } else if (value->isValueNullType()) {
     os << "null";
   }
   else {
--- cpp/jsonTest.cc
+++ cpp/jsonTest.cc
@@ -6,31 +6,31 @@
 int main() {
   {
     json j(23);
-    std::string str = j.dump();
+    std::string str = j.dumps();
     assert(str == "23");
   }
   {
     std::string s{"hello"};
     json j(s);
-    std::string str = j.dump();
+    std::string str = j.dumps();
     assert(str == "hello");
   }
   {
     bool val = true;
     json j(val);
-    std::string  str = j.dump();
+    std::string  str = j.dumps();
     assert(str == "true");
   }
   {
     bool val = false;
     json j(val);
-    std::string  str = j.dump();
+    std::string  str = j.dumps();
     assert(str == "false");
   }
   {
     json::nullType val = json::nullValue;
     json j(val);
-    std::string str = j.dump();
+    std::string str = j.dumps();
     assert(str == "null");
   }
   {
@@ -39,7 +39,7 @@ int main() {
     std::map<std::string, std::vector<int>> obj{{"name1", arr1}, {"name2", arr2}};
     json j = obj;
     j.insert(std::make_pair(std::string{"name3"}, 43));
-    std::string str = j.dump();
-    assert(str == "{{name1 : [ 23,  23,  4,  545,  4]}, {name2 : [ 2,  3,  4,  45,  4]}, {name3 : 43}}");
+    std::string str = j.dumps();
+    assert(str == "{{name1: [ 23,  23,  4,  545,  4]}, {name2: [ 2,  3,  4,  45,  4]}, {name3: 43}}");
   }
 }
